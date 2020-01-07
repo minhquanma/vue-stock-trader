99a96760ee45ddf9119c78b57580f03d1b51360e
minhquanma <minhquanma@gmail.com>
2020-01-07 22:44:40 +0700

    Update

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -1,61 +1,70 @@
 <template>
-    <div class="col-md-4 my-container">
-        <div class="card border-success">
-            <div class="card-header">
-                <h4>
-                    {{ stock.name }}
-                    <small>(Price: {{ stock.price }} | Quantity: {{ stock.quantity }})</small>
-                </h4>
-            </div>
-            <div class="card-body">
-                <div class="panel-body">
-                    <div class="float-left">
-                        <input
-                                type="number"
-                                class="form-control"
-                                placeholder="Quantity"
-                                v-model="quantity"
-                        />
-                    </div>
-                    <div class="float-right">
-                        <button
-                                class="btn btn-success"
-                                @click="sellStock"
-                                :disabled="quantity <= 0"
-                        >Sell</button>
-                    </div>
-                </div>
-            </div>
+  <div class="col-md-4 my-container">
+    <div class="card border-success">
+      <div class="card-header">
+        <h4>
+          {{ stock.name }}
+          <small>(Price: {{ stock.price }} | Quantity: {{ stock.quantity }})</small>
+        </h4>
+      </div>
+      <div class="card-body">
+        <div class="panel-body">
+          <div class="float-left">
+            <input
+              type="number"
+              class="form-control"
+              placeholder="Quantity"
+              v-model="quantity"
+              :class="{ danger: insufficientQuantity}"
+            />
+          </div>
+          <div class="float-right">
+            <button
+              class="btn btn-success"
+              @click="sellStock"
+              :disabled="insufficientQuantity ||  quantity <= 0"
+            >{{ insufficientQuantity ? 'Not enough' : 'Sell' }}</button>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
-<script>
-    import {mapActions} from "vuex";
-    import {OrderModel} from "@/data/order";
-
-    export default {
-        props: ['stock'],
-        data() {
-            return {
-                quantity: 0
-            }
-        },
-        methods: {
-            ...mapActions({
-                placeSellOrder: 'sellStock'
-            }),
-            sellStock() {
-                const order = {
-                    stockId: this.stock.id,
-                    stockPrice: this.stock.price,
-                    quantity: this.quantity
-                };
-                this.placeSellOrder(order);
-                this.quantity = 0;
-            }
-        }
-    }
-</script>
-<style>
-
+<style scoped>
+.danger {
+  border: 1px solid red;
+}
 </style>
+<script>
+import { mapActions } from "vuex";
+import { OrderModel } from "@/data/order";
+
+export default {
+  props: ["stock"],
+  data() {
+    return {
+      quantity: 0
+    };
+  },
+  computed: {
+    insufficientQuantity() {
+      return this.quantity > this.stock.quantity;
+    }
+  },
+  methods: {
+    ...mapActions({
+      placeSellOrder: "sellStock"
+    }),
+    sellStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      };
+      this.placeSellOrder(order);
+      this.quantity = 0;
+    }
+  }
+};
+</script>
+
--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,60 +1,70 @@
 <template>
-    <div class="col-md-4 my-container">
-        <div class="card">
-            <div class="card-header">
-                <h4>
-                    {{ stock.name }}
-                    <small>(Price: {{ stock.price }})</small>
-                </h4>
-            </div>
-            <div class="card-body">
-                <div class="panel-body">
-                    <div class="float-left">
-                        <input
-                                type="number"
-                                class="form-control"
-                                placeholder="Quantity"
-                                v-model="quantity"
-                        />
-                    </div>
-                    <div class="float-right">
-                        <button
-                                class="btn btn-success"
-                                @click="buyStock"
-                                :disabled="quantity <= 0"
-                        >Buy</button>
-                    </div>
-                </div>
-            </div>
+  <div class="col-md-4 my-container">
+    <div class="card">
+      <div class="card-header">
+        <h4>
+          {{ stock.name }}
+          <small>(Price: {{ stock.price }})</small>
+        </h4>
+      </div>
+      <div class="card-body">
+        <div class="panel-body">
+          <div class="float-left">
+            <input
+              type="number"
+              class="form-control"
+              placeholder="Quantity"
+              v-model="quantity"
+              :class="{ danger: insufficientFunds }"
+            />
+          </div>
+          <div class="float-right">
+            <button
+              class="btn btn-success"
+              @click="buyStock"
+              :disabled="insufficientFunds || quantity <= 0"
+            >{{ insufficientFunds ? 'Insufficient Funds' : 'Buy' }}</button>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 <script lang="ts">
+import { OrderModel } from "@/data/order";
 
-    import {OrderModel} from "@/data/order";
-
-    export default {
-        props: ['stock'],
-        data() {
-            return {
-                quantity: 0
-            }
-        },
-        methods: {
-            buyStock(): void {
-                const order: OrderModel = {
-                    stockId: this.stock.id,
-                    stockPrice: this.stock.price,
-                    quantity: this.quantity
-                };
-
-                this.$store.dispatch('buyStock', order);
-
-                this.quantity = 0;
-            }
-        }
+export default {
+  props: ["stock"],
+  data() {
+    return {
+      quantity: 0
+    };
+  },
+  computed: {
+    funds() {
+      return this.$store.getters.funds;
+    },
+    insufficientFunds() {
+      return this.quantity * this.stock.price > this.funds;
     }
-</script>
-<style>
+  },
+  methods: {
+    buyStock(): void {
+      const order: OrderModel = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      };
 
+      this.$store.dispatch("buyStock", order);
+
+      this.quantity = 0;
+    }
+  }
+};
+</script>
+<style scoped>
+.danger {
+  border: 1px solid red;
+}
 </style>
